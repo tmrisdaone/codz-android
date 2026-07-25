7d129e88a1b941fa3def4a3c82e499023d33e540
StudyBuddy <studybuddy@example.com>
2026-07-24 23:50:28 -0400

    Fix root build.gradle.kts: add buildscript block with plugin repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
